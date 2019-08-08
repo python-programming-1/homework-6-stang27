c083a323fdf14cd5cdf2584d97294fb8d2f99ef9
stang <sunnytang2727@gmail.com>
2019-08-07 18:46:12 -0700

    updated with comments

--- videos.py
+++ videos.py
@@ -39,50 +39,58 @@ def print_data(data):
 
 def get_most_popular_and_least_popular_channel(data):
     """ fill in the Nones for the dictionary below using the vid data """
+    # Create an empty dictionary that will take the channel and the views
     channels_and_views = {}
+    # Iterate through the loop and sum the views for the same channel name
     for video in data[1:]:
+    	# Set the default for the views dictionary
     	channels_and_views.setdefault(video['channel_title'], 0)
     	channels_and_views[video['channel_title']] += int(video['views'])
     
+    # Create the return dictionary
     most_popular_and_least_popular_channel = {'most_popular_channel': None, 'least_popular_channel': None, 'most_pop_num_views': 0,
                                               'least_pop_num_views': float('Inf')}
+    # Analyze the view dictionary for the most popular channel
     for k,v in channels_and_views.items():
     	if int(v) > most_popular_and_least_popular_channel['most_pop_num_views']:
     		most_popular_and_least_popular_channel['most_popular_channel'] = k
     		most_popular_and_least_popular_channel['most_pop_num_views'] = int(v)
-
+    # Analyze the view dictionary for the least popular channel
     for k,v in channels_and_views.items():
     	if k == 'Unspecified':
     		continue
     	if int(v) < most_popular_and_least_popular_channel['least_pop_num_views']:
     		most_popular_and_least_popular_channel['least_popular_channel'] = k
     		most_popular_and_least_popular_channel['least_pop_num_views'] = v
-
+    # Return dictionary
     return most_popular_and_least_popular_channel
 
 
 def get_most_liked_and_disliked_channel(data):
     """ fill in the Nones for the dictionary below using the bar party data """
+    # Create 2 dictionarys: 1 for likes, 1 for dislikes
     channels_and_likes = {}
     channels_and_dislikes = {}
+    # Iterate through the loop and sum the likes and sum the dislikes
     for video in data[1:]:
+    	# Set the default for the likes and dislikes
     	channels_and_likes.setdefault(video['channel_title'], 0)
     	channels_and_dislikes.setdefault(video['channel_title'], 0)
     	channels_and_likes[video['channel_title']] += int(video['likes'])
     	channels_and_dislikes[video['channel_title']] += int(video['dislikes'])
-
+    # Create the return dictionary
     most_liked_and_disliked_channel = {'most_liked_channel': None, 'num_likes': 0, 'most_disliked_channel': None, 'num_dislikes': 0}
-
+    #Analyze the likes dictionary for the most likes
     for k,v in channels_and_likes.items():
     	if int(v) > most_liked_and_disliked_channel['num_likes']:
     		most_liked_and_disliked_channel['most_liked_channel'] = k
     		most_liked_and_disliked_channel['num_likes'] = int(v)
-
+    # Analyze the dislikes dictionary for the most dislikes
     for k,v in channels_and_dislikes.items():
     	if int(v) > most_liked_and_disliked_channel['num_dislikes']:
     		most_liked_and_disliked_channel['most_disliked_channel'] = k
     		most_liked_and_disliked_channel['num_dislikes'] = int(v)
-
+    # Return dictionary
     return most_liked_and_disliked_channel
 
 
